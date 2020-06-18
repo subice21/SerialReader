e33f95c038df3dd222f3f1857a3da592388ea8e4
subice21 <40129011+subice21@users.noreply.github.com>
2020-06-18 11:08:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "Serial.cpp"
+#include <iostream>
+#include <windows.h>
+int main(){
+
+    CSerial serial;
+    int port;
+    int baund;
+    int bufferI;
+
+    std::cout << "port?" <<std::endl;
+    std::cin >> port;
+    std::cout << "baund ?"<< std::endl;
+    std::cin>> baund;
+    std::cout << "what size the buffer ?"<< std::endl;
+    std::cin>> bufferI;
+    
+
+    if (serial.Open(port, baund)){
+        for(;;){
+        Sleep(1000);
+        char* lpBuffer = new char[bufferI];
+        int nBytesRead = serial.ReadData(lpBuffer, bufferI);
+        printf(lpBuffer);
+        delete []lpBuffer;
+        }
+       
+    }
+    else
+        std::cout << "Failed to open port!" << std::endl;
+
+
+    return 0;
+}
---
+++ stdafx.h
@@ -0,0 +1,8 @@
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <mmsystem.h>
